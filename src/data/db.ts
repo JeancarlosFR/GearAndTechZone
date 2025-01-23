9d22a7f82af9abdabb732325e9723a1dd1ed475d
Jean Carlos <je.franco@duocuc.cl>
2025-01-23 01:52:46 -0300

    Se agrega plantilla de diseño, estructura de proyecto  y se muestra los productos

---
+++ src/data/db.ts
@@ -0,0 +1,44 @@
+export const db = [
+    {
+        id: 1,
+        name: 'Razer Barracuda',
+        image: 'producto_01',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: 299,
+    },
+    {
+        id: 2,
+        name: 'Mouse Logitech',
+        image: 'producto_02',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: 349,
+    },
+    {
+        id: 3,
+        name: 'Machenike Keyboard',
+        image: 'producto_03',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: 329,
+    },
+    {
+        id: 4,
+        name: 'PS5 Controller',
+        image: 'producto_04',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: 299,
+    },
+    {
+        id: 5,
+        name: 'PS5',
+        image: 'producto_05',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: 399,
+    },
+    {
+        id: 6,
+        name: 'Monitor LG',
+        image: 'producto_06',
+        description: 'Morbi ornare augue nisl, vel elementum dui mollis vel. Curabitur non ex id eros fermentum hendrerit.',
+        price: 329,
+    }
+  ]
